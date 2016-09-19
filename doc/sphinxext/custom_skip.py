eb1afb772ccfacd875efa8c5f96ed96f296aef58
Lexi Brent <lexi@lexibrent.com>
2016-09-19 12:57:10 +1000

    Fix bug in custom_skip Sphinx ext

--- doc/sphinxext/custom_skip.py
+++ doc/sphinxext/custom_skip.py
@@ -5,13 +5,17 @@ def autodoc_skip_protected(app, what, name, obj, skip, options):
   Returns False if obj is callable and starts with _ and does not contain __ in
   its name. We want Sphinx to document our "protected" (soft private) methods.
   """
-  return not(
-    skip and
+  if not skip:
+    return False
+
+  include = (
     callable(obj) and
     "__" not in name and
     name.startswith("_") and
     inspect.getdoc(obj) is not None
   )
+
+  return not include
 
 def setup(app):
   app.connect("autodoc-skip-member", autodoc_skip_protected)
