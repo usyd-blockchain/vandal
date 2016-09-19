ede8d8721c100898061baa0f3e6c184008a85560
latiosu <latiosworks@gmail.com>
2016-09-19 22:37:12 +1000

    Refactoring output of create_send_test.sh

--- test/tsv/create_send_test.sh
+++ test/tsv/create_send_test.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+# ------------------ #
+# Usage instructions #
+# ------------------ #
+
 if [ $# -ne 1 ]
 then
     echo "Creates a checked/unchecked send test with given solidity contract."
@@ -9,27 +13,37 @@ then
     exit
 fi
 
+
+# --------------------- #
+# Generate helper files #
+# --------------------- #
+
+format="%-50s...\n"
 filename=$(basename "$1")
 filename="${filename%.*}"
 
 # Use directory with filename as output for test case
 if [ -d "$filename" ];
 then
-    echo "Directory already exists. Using ..."
+    printf $format "Directory already exists, overwriting instead"
 else
-    echo "Creating a new directory called $filename/ ..."
+    printf $format "Creating a new directory called $filename/"
     mkdir $filename
 fi
 
+# Generate supporting folders
+printf $format "Creating directories for logical relations"
+(cd $filename; mkdir lib; mkdir facts)
+
 # Generate temporary directory with binary output of solidity compiler (bytecode)
-echo "Generating bytecode from Solidity compiler ..."
+printf $format "Generating bytecode from Solidity compiler"
 solc --optimize --optimize-runs 300000 --bin -o tempbin $1
 
 # Copy input Solidity contract into new directory
 cp $1 $filename
 
 # Generate disasm output and save into new directory
-echo "Generating disasm output ..."
+printf $format "Generating disasm output"
 for bytecode in tempbin/*.bin
 do
     sed -i '' -e '$a\' $bytecode
@@ -37,28 +51,39 @@ do
 done
 
 # Generate TAC output and save into new directory
-echo "Generating TAC output ..."
+printf $format "Generating TAC output"
 (cd $filename; ../../../bin/decompile $filename.dasm > $filename.tac)
 
 # Generate .facts files for Souffle in new directory
-echo "Generating fact files for Souffle ..."
-(cd $filename; ../../../bin/decompile2 $filename.dasm)
+printf $format "Generating fact files for Souffle"
+(cd $filename/facts; ../../../../bin/decompile2 ../$filename.dasm)
 
 # Copy the checked/unchecked send Datalog template with new filename to new directory
-echo "Copying over template test ..."
-cp checked-send/library.dl $filename/
-cp checked-send/checked-send.dl $filename/$filename.dl
+printf $format "Copying over analysis library"
+cp checked-send/lib/* $filename/lib/
 
 # Generatae cfg.dot file in new directory
-echo "Generating .dot file for graph generation ..."
+printf $format "Generating .dot file for graph generation"
 (cd $filename; ../../../bin/decompile3 $filename.dasm)
 
 # Generate PDF of the control flow graph in new directory
-echo "Generating PDF of control flow graph ..."
+printf $format "Generating PDF of control flow graph"
 dot -Tpdf $filename/cfg.dot -o $filename/cfg.pdf
 
+
+# --------- #
+# Run tests #
+# --------- #
+printf $format "Running tests via Souffle"
+(cd $filename; souffle -D- -Ffacts lib/checked-send.dl > results.txt)
+
+
+# -------- #
+# Clean up #
+# -------- #
+
 # Remove temporary files
-echo "Cleaning up ..."
+printf $format "Cleaning up"
 rm -r tempbin
 rm $filename/cfg.dot
 
