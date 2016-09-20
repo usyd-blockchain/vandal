b049faccea866a3a71e6f6486c72a2b79f2a14f8
Lexi Brent <lexi@lexibrent.com>
2016-09-20 21:03:51 +1000

    Update test_cfg to use fixtures for blocks and graph creation

--- test/test_cfg.py
+++ test/test_cfg.py
@@ -1,7 +1,9 @@
 import pytest
+
+import patterns
 import cfg
 
-@pytest.fixture(scope="module", params=[
+@pytest.fixture(params=[
   (1,5),
   (12213,72728),
   (-12312313,-1),
@@ -13,54 +15,57 @@ def basicblock(request):
       super().__init__(entry, exit)
   return SubBlock(*request.param), request.param
 
+@pytest.fixture
+def graph(request):
+  class SubCFG(cfg.ControlFlowGraph):
+    def __init__(self):
+      super().__init__()
+  return SubCFG()
+
+#### Tests: ####
 
 class TestBasicBlock:
-
   def test_construction(self, basicblock):
     b, (en, ex) = basicblock
     assert b.entry == en
     assert b.exit == ex
-    assert b.preds == []
-    assert b.succs == []
-    assert not b.has_unresolved_jump
+    assert b.preds == [], "preds must start empty"
+    assert b.succs == [], "succs must start empty"
+    assert not b.has_unresolved_jump, "has_unresolved_jump must start as False"
 
   def test_str(self, basicblock):
     b, param = basicblock
-    assert type(str(b)) is str
+    assert type(str(b)) is str, "__str__ must return a string"
 
   def test_ident(self, basicblock):
     b, param = basicblock
-    assert b.ident().startswith("0x") or
-           b.ident().startswith("-0x")
+    assert (b.ident().startswith("0x") or
+            b.ident().startswith("-0x")), "ident() must return hex string"
 
 
 class TestControlFlowGraph:
-  def test_construction(self):
-    class SubCFG(cfg.ControlFlowGraph):
-      pass
+  def test_construction(self, graph):
+    assert graph.blocks == []
+    assert graph.root is None
 
-    self.g = SubCFG()
-    assert self.g.blocks == []
-    assert self.g.root is None
-
-  def test_accept(self):
-    class SubCFGVisitor:
+  def test_accept(self, graph):
+    class SubCFGVisitor(patterns.Visitor):
       def __init__(self):
         self.visited = []
       def visit(obj):
         visited.append(obj)
 
     v = SubCFGVisitor()
-    self.g.accept(v)
-    assert self.g in v.visited
+    assert len(v.visited) == 0
+    graph.accept(v)
+    # TODO
 
-  def test_edge_list(self):
-    assert len(self.g.edge_list()) == 0
+  def test_edge_list(self, graph):
+    assert len(graph.edge_list()) == 0
 
-  def test_str(self):
-    assert type(str(self.g)) is str
-    assert len(str(self.g)) > 0
+  def test_str(self, graph):
+    assert type(str(graph)) is str
 
-  def test_len(self):
-    assert type(len(self)) is int
-    assert len(self.g) == len(self.g.blocks)
+  def test_len(self, graph):
+    assert type(len(graph)) is int
+    assert len(graph) == len(graph.blocks)
