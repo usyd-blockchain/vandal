dcfa2088a73e11562ab85b67b5172ec6d037feaf
Lexi Brent <lexi@lexibrent.com>
2016-09-20 20:16:46 +1000

    Refactor CFG tests to use fixture in-progress

--- test/test_cfg.py
+++ test/test_cfg.py
@@ -1,33 +1,66 @@
+import pytest
 import cfg
 
-class SubBlock(cfg.BasicBlock):
-  def __init__(self, entry, exit):
-    super().__init__(entry, exit)
+@pytest.fixture(scope="module", params=[
+  (1,5),
+  (12213,72728),
+  (-12312313,-1),
+  (99932478932874293434,-128918723982173),
+])
+def basicblock(request):
+  class SubBlock(cfg.BasicBlock):
+    def __init__(self, entry, exit):
+      super().__init__(entry, exit)
+  return SubBlock(*request.param), request.param
+
 
 class TestBasicBlock:
-  # TODO: Make this a fixture
-  b = SubBlock(1,5)
 
-  def test_construction(self):
-    assert self.b.entry == 1
-    assert self.b.exit == 5
-    assert self.b.preds == []
-    assert self.b.succs == []
-    assert not self.b.has_unresolved_jump
+  def test_construction(self, basicblock):
+    b, (en, ex) = basicblock
+    assert b.entry == en
+    assert b.exit == ex
+    assert b.preds == []
+    assert b.succs == []
+    assert not b.has_unresolved_jump
 
-  def test_str(self):
-    assert type(str(self.b)) == str
+  def test_str(self, basicblock):
+    b, param = basicblock
+    assert type(str(b)) is str
 
-  def test_ident(self):
-    assert self.b.ident().startswith("0x")
+  def test_ident(self, basicblock):
+    b, param = basicblock
+    assert b.ident().startswith("0x") or
+           b.ident().startswith("-0x")
 
-
-class SubCFG(cfg.ControlFlowGraph):
-  def __init__(self):
-    super().__init__()
 
 class TestControlFlowGraph:
-  g = SubCFG()
-
   def test_construction(self):
-    pass
+    class SubCFG(cfg.ControlFlowGraph):
+      pass
+
+    self.g = SubCFG()
+    assert self.g.blocks == []
+    assert self.g.root is None
+
+  def test_accept(self):
+    class SubCFGVisitor:
+      def __init__(self):
+        self.visited = []
+      def visit(obj):
+        visited.append(obj)
+
+    v = SubCFGVisitor()
+    self.g.accept(v)
+    assert self.g in v.visited
+
+  def test_edge_list(self):
+    assert len(self.g.edge_list()) == 0
+
+  def test_str(self):
+    assert type(str(self.g)) is str
+    assert len(str(self.g)) > 0
+
+  def test_len(self):
+    assert type(len(self)) is int
+    assert len(self.g) == len(self.g.blocks)
