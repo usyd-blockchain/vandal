06549faf82d7f8e48a2b957b1e10ef428119fd0e
Jack Feng <huadong.feng@outlook.com>
2020-01-13 15:33:12 -0600

    Tried logging to double check number of edges and block were parsed correctly.

--- bin/generate_cfg.py
+++ bin/generate_cfg.py
@@ -2,9 +2,13 @@ import src.exporter as exporter
 import src.dataflow as dataflow
 import src.tac_cfg as tac_cfg
 import src.settings as settings
+#import logging
 
 
 def vandal_cfg(input):
+    # logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+    # logging.getLogger().setLevel(logging.INFO)
+    # logging.info("asd")
     settings.import_config(settings._CONFIG_LOC_)
     cfg = tac_cfg.TACGraph.from_bytecode(input)
 
