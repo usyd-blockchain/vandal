785854a04664fe128f08a6f320023f364dffde58
Jack Feng <huadong.feng@outlook.com>
2020-09-28 01:53:55 -0500

    commandline execution cannot look up module

--- bin/generate_cfg.py
+++ bin/generate_cfg.py
@@ -1,8 +1,16 @@
+import sys
+from os.path import abspath, dirname, join
+
+src_path = join(dirname(abspath(__file__)), "..")
+sys.path.insert(0, src_path)
+
 import src.exporter as exporter
 import src.dataflow as dataflow
 import src.tac_cfg as tac_cfg
 import src.settings as settings
-#import logging
+
+
+# import logging
 
 
 def vandal_cfg(input):
@@ -18,5 +26,3 @@ def vandal_cfg(input):
 
     res = exporter.CFGStringExporter(cfg).export()
     return res
-
-
