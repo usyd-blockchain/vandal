a181f7a948beb28abc6686524b5a8c9e0df43ef5
Jack Feng <huadong.feng@outlook.com>
2021-01-20 15:47:29 -0600

    using kwargs

--- bin/generate_cfg.py
+++ bin/generate_cfg.py
@@ -3,6 +3,7 @@ from os.path import abspath, dirname, join
 
 src_path = join(dirname(abspath(__file__)), "..")
 sys.path.insert(0, src_path)
+# sys.path.append('C:\\Users\\Administrator\\Documents\\SourceTree\\ctfuzz\\ctfuzz\\static_analysis\\core\\vandal\\bin\\..')
 
 import src.exporter as exporter
 import src.dataflow as dataflow
@@ -13,12 +14,12 @@ import src.settings as settings
 # import logging
 
 
-def vandal_cfg(input):
+def vandal_cfg(**kwargs):
     # logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
     # logging.getLogger().setLevel(logging.INFO)
     # logging.info("asd")
     settings.import_config(settings._CONFIG_LOC_)
-    cfg = tac_cfg.TACGraph.from_bytecode(input)
+    cfg = tac_cfg.TACGraph.from_bytecode(kwargs['input'])
 
     settings.import_config(settings._CONFIG_LOC_)
 
